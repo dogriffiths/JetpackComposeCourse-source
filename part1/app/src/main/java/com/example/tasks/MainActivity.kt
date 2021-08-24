d2ae90166c40c566454bacbb8cfa84974f48ac3a
David Griffiths <dogriffiths@gmail.com>
2021-08-24 11:08:31 +0100

    feat: Add task summary

--- part1/app/src/main/java/com/example/tasks/MainActivity.kt
+++ part1/app/src/main/java/com/example/tasks/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.tasks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tasks.TaskSummary
 import com.example.tasks.ui.theme.TasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +23,11 @@ class MainActivity : ComponentActivity() {
             TasksTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column {
+                        TaskSummary("Buy bread", true)
+                        TaskSummary("Buy milk", true)
+                        TaskSummary("Attend yoga", true)
+                    }
                 }
             }
         }
@@ -25,14 +35,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskSummary(name: String, complete: Boolean) {
+    Surface(color = MaterialTheme.colors.background) {
+        Row(
+            modifier = Modifier.padding(4.dp).fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(name, modifier = Modifier.align(Alignment.CenterVertically))
+            Checkbox(checked = complete, onCheckedChange = {})
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    TasksTheme {
-        Greeting("Android")
-    }
+fun PreviewTaskSummary() {
+    TaskSummary("Buy bread", true)
 }
